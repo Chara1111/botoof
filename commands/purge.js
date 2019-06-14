b44421b53f40f23c687df1a4a329f0bc35adf632
Kirill Babikov <kirill.babikov28@gmail.com>
2019-06-14 20:43:42 +0300

    fix of purge when it deleted 1 less message

--- commands/purge.js
+++ commands/purge.js
@@ -3,13 +3,13 @@ exports.run = async(client, message, args) => {
     const emb = new embed()
         .setTitle('Oops!')
         .setDescription('<:cross:584800355951443968> To purge the channel you need to have manage messages permission!')
-        .setColor('#f00');
+        .setColor('#ff0000');
     if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(emb);
 
     const deleteCount = parseInt(args[0], 10);
 
     if(!deleteCount || deleteCount < 1 || deleteCount > 100) return message.reply("Please provide a number between 1 and 100 for the number of messages to delete");
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    const fetched = await message.channel.fetchMessages({limit: deleteCount + 1});
     message.channel.bulkDelete(fetched)
         .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
 };
