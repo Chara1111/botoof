b2bb50d6642dc9fd4b08c31eb528e78288de141c
Kirill Babikov <kirill.babikov28@gmail.com>
2019-06-17 20:56:10 +0300

    remove devsuggest

--- commands/devsuggest.js
+++
@@ -1,9 +0,0 @@
-exports.run = (client, message, args) => {
-    client.users.get('427007456502218752').send(`New dev suggestion: 
-${args.join(' ')}
-   
-   
-   
-As suggested by ${message.author.tag}(of user ID ${message.author.id})`);
-    message.channel.send('Suggestion successfully sent.')
-};
