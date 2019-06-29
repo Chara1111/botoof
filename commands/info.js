928dc28ea72e16d4e74e72c75ee456e04f5267eb
Chara1111 <kirill.babikov28@gmail.com>
2019-06-29 23:27:39 +0300

    1.0.5 typo fix

--- commands/info.js
+++ commands/info.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
     const emb = require('discord.js').RichEmbed;
     const embed = new emb()
         .setTitle('Current version 1.0.5')
-        .setDescription('Version 1.0.4 =>' +
+        .setDescription('Version 1.0.5 =>' +
         '\n**ok**' +
         '\n> thanks everyone who paid respects to currency using `>f` command' +
         '\n> added automod' +
