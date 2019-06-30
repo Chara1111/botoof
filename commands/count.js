bc11794b619cf5bc7de27ffeffeadf73097c16e4
Chara1111 <kirill.babikov28@gmail.com>
2019-06-30 20:46:37 +0300

    1.0.5 big changes to count (added cache, now there cannot be few counts at once)

--- commands/count.js
+++ commands/count.js
@@ -1,4 +1,10 @@
+const fs = require('fs');
+
 exports.run = async(client, message, args) => {
+    let cache = JSON.parse(fs.readFileSync("../cache.json", "utf8"));
+    if(cache.count.active) return message.reply(`There is already active count! Some information:
+Count is in server \`${client.guilds.get(cache.count.guild).name}\`, channel <#${cache.count.channel}>. Time was set is ${cache.count.time}. User who called the command is ${cache.count.user}`);
+
     let countfrom = args[0];
     if(Number(countfrom) === undefined || isNaN(Number(countfrom))) countfrom = 60;
     countfrom = Number(countfrom);
@@ -9,6 +15,17 @@ exports.run = async(client, message, args) => {
     const d = require('discord.js').RichEmbed;
     const e = new d()
         .setDescription(countfrom);
+
+    // Filling Cache with information
+
+    cache.count.active = true;
+    cache.count.guild = message.guild.id;
+    cache.count.channel = message.channel.id;
+    cache.count.time = countfrom;
+    cache.count.user = message.author.tag;
+
+    fs.writeFile("../cache.json", JSON.stringify(cache), (err) => {if (err) console.log(err)});
+
     message.channel.send(e)
         .then(async(msg) => {
             for(let i=countfrom; i>-1; i--) {
--- commands/f.js
+++ commands/f.js
@@ -1,4 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send(`**${message.author.tag}**, you just paid respects to demoted RoDog <a:acry:587585991175438347>
-.`)
+    message.channel.send(`**${message.author.tag}**, you just paid respects to demoted RoDog <a:acry:587585991175438347>.`)
 };
