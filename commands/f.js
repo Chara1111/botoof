bd3dd66ed91a0734e2433b26405d87709a5dc9f9
Chara1111 <kirill.babikov28@gmail.com>
2019-06-30 20:22:05 +0300

    1.0.5 pay respects to RoDog

--- commands/f.js
+++ commands/f.js
@@ -1,3 +1,4 @@
 exports.run = (client, message, args) => {
-    message.channel.send(`**${message.author.tag}**, we are not paying respects to anything right now.`)
+    message.channel.send(`**${message.author.tag}**, you just paid respects to demoted RoDog <a:acry:587585991175438347>
+.`)
 };
