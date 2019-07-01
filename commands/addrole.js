de9d8b967dbb56d42de0ccc0be73a5711be17004
Chara1111 <kirill.babikov28@gmail.com>
2019-07-01 20:57:48 +0300

    1.0.5 addrole and removerole fixes

--- commands/addrole.js
+++ commands/addrole.js
@@ -4,10 +4,14 @@ exports.run = async(client, message, args) => {
     const embed = new emb()
         .setTitle('Uh oh!')
         .setDescription('To assign roles to members, you need to have manage roles permission!')
-        .setColor('#f00');
+        .setColor('#ff0000');
     if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(embed);
-    if(!args[1]) return message.channel.send('Please mention valid user and role name.');
-    let role = message.guild.roles.find('name', args[1]);
+    if(!args[0]) return message.channel.send('Please mention valid user.');
+    let role;
+    if(message.mentions.roles.first()) let role = message.mentions.roles.first();
+    else if(message.guild.roles.get(args[1])) let role = message.guild.roles.get(args[1]);
+    else if(message.guild.roles.find('name', args[1])) let role = message.guild.roles.find('name', args[1]);
+    else return message.channel.send("Please mention valid role/say it name/id");
     if(message.member.highestRole.comparePositionTo(role) < 1) return message.channel.send('Uh oh, to assign roles, they need to be below your highest role!');
     let user = message.mentions.users.first().id;
     user = message.guild.members.get(user);
