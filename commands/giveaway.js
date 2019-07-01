e1d8fe180195758b054c1ed90f86ad437fd501b9
Chara1111 <kirill.babikov28@gmail.com>
2019-07-01 15:18:29 +0300

    1.0.5 added >giveaway end

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -13,10 +13,27 @@ exports.run = async(client, message, args) => {
         .setTitle('Oops!')
         .setFooter('bot created by dank_meme#0001')
         .setDescription('<:cross:584800355951443968> To start giveaways, you need manage channels permission!');
-    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send(fail);
+    if(!message.member.hasPermission("MANAGE_CHANNELS") && message.member.roles.get('')) return message.channel.send(fail);
 
+    let cache = JSON.parse(fs.readFileSync("./cache.json", "utf8"));
+
+    if(cache.giveaway.active) return message.reply(`There is already active giveaway there! It is in guild \`${client.guilds.get(cache.giveaway.guild).name}\`, channel <#${cache.giveaway.channel}>. Time was set is ${cache.giveaway.time} hours. Item to giveaway was \`${cache.giveaway.item}\` with ${cache.giveaway.winners} winners`);
     //patterns
     let patterns = {'hourly500': {'time': 1, 'item': '500 hourly dank memer coins', 'winners': 1}};
+
+    //fill cache function
+
+    async function fillcache(active, gid, chid, hours, item, cacheobject, mid, wnrs) {
+        cacheobject.giveaway.active = active;
+        cacheobject.giveaway.guild = gid;
+        cacheobject.giveaway.channel = chid;
+        cacheobject.giveaway.time = hours;
+        cacheobject.giveaway.item = item;
+        cacheobject.giveaway.mid = mid;
+        cacheobject.giveaway.winners = wnrs
+
+        await fs.writeFile("./cache.json", JSON.stringify(cacheobject), (err) => {if (err) console.log(err)});
+    }
 
     //start function
     async function gstart(hours, winners, item) {
@@ -29,9 +46,14 @@ exports.run = async(client, message, args) => {
             .setFooter('Good luck, react to 🎉 to participate.');
 
         await message.channel.send(giveaway).then(async msg => {
+            await fillcache(true, message.guild.id, message.channel.id, hours, item, cache, msg.id, winners);
             await msg.react('🎉');
 
             setTimeout(async function() {
+                if(!cache.giveaway.active) return;
+
+                await fillcache(false, "guild id", "channel id", "hours", "item to win", cache, "message id", "amount of winners");
+
                 let reacted = msg.reactions.filter(rx => rx.emoji.name === '🎉').first().users.array();
                 let wonusers = [];
 
@@ -58,8 +80,6 @@ exports.run = async(client, message, args) => {
 
 
     if(!args[0]) return message.reply(usage);
-    if(args[0] !== 'start' && args[0] !== 'pattern') return message.reply(usage);
-
 
     message.delete();
 
@@ -86,4 +106,44 @@ exports.run = async(client, message, args) => {
         await gstart(hours, winners, item)
 
     }
+
+    else if(args[0] === 'end') {
+        if(!cache.giveaway.active) return message.reply("There is no currently running giveaway.");
+
+        let channel = client.channels.get(cache.giveaway.channel);
+        if(!channel) return;
+
+        let gmsg = channel.fetchMessage(cache.giveaway.mid);
+        if(!gmsg) return;
+
+        let winners = cache.giveaway.winners;
+        let item = cache.giveaway.item;
+
+
+
+        await fillcache(false, "guild id", "channel id", "hours", "item to win", cache, "message id", "amount of winners");
+
+        let reacted = gmsg.reactions.filter(rx => rx.emoji.name === '🎉').first().users.array();
+        let wonusers = [];
+
+        for(let i=0; i<winners; i++) {
+            let id = reacted[Math.floor(Math.random() * reacted.length)];
+
+            if(id.id === '579715446123790366') return;
+            wonusers.push('<@!' + id.id + '>')
+        }
+
+        let won = wonusers.join(', ');
+
+        const ended = new embed()
+            .setTitle(':tada: Giveaway ended! :tada:')
+            .setColor('#00ff00')
+            .setDescription(`Winner(s): ${won}`)
+            .setFooter('Good luck next time!');
+        await gmsg.edit(ended);
+
+        gmsg.channel.send('Congratulations, ' + won + ', you won ' + item)
+    }
+
+    else return message.reply(usage)
 };
