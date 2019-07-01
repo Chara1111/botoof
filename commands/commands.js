db9460c635dc5540cf203f97f5b241384fa8cdbd
Chara1111 <kirill.babikov28@gmail.com>
2019-07-01 22:48:52 +0300

    1.0.6 fully done

--- commands/commands.js
+++ commands/commands.js
@@ -1,84 +1,85 @@
 let discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-    message.channel.send("WARNING, this menu is being reworked soon, I am not longer working on **this** version so some commands can be missing.")
-    let pr = '>';
-    const embed = new discord.RichEmbed()
-        .setTitle('Please choose the category for commands.')
-        .addField('🔨 Staff Commands', `\`${pr}commands mod\``)
-        .addField('🎲 Regular Commands', `\`${pr}commands regular\``)
-        .addField('💵 Currency Commands', `\`${pr}commands currency\``)
-        .addField('🎱 Fun Commands', `\`${pr}commands fun\``)
-        .setColor('#2d2d2d');
-    if(!args[0]) return message.channel.send(embed);
+    let usage = new discord.RichEmbed()
+        .setTitle('Choose command menu')
+        .setDescription('You will see commands with their usage, and example')
+        .setColor('#4764fa')
+        .addField('🔨 Moderator commands', '`>cmds mod`') // addrole, ban, kick, mute, purge, removerole, unmute
+        .addField('🎲 Fun commands', '`>cmds fun`') // ask, b, count, dice, f, flex, meme, scam
+        .addField('🔧 Server-related commands', '`>cmds server`') // announce, giveaway, gp, serverinfo, suggest
+        .addField('🤖 Bot-related commands', '`>cmds bot`') // commands, help, info, ping, uptime
+        .addField('💯 Regular commands', '`>cmds regular`') // avatar, userinfo
+        .setFooter('You are seeing this because probably specified wrong category or didn\'t specify it at all.'); //
+    if(!args[0]) return message.reply(usage);
 
     if(args[0] === 'mod') {
-        const embed2 = new discord.RichEmbed()
-            .setTitle('🔨 Staff commands')
-            .setColor('#387248')
-            .setFooter('bot created by dank_meme#0001')
-            .setDescription('<> - required, [] - optional')
-            .addField(`${pr}ban <@mention/user id> <reason>`, 'Bans specified person for given reason. Need ban members permission.')
-            .addField(`${pr}kick <@mention/user id> <reason>`, 'Kicks specified person for given reason. Need kick members permission.')
-            .addField(`${pr}announce <announcement text>`, 'Announce something in current channel. Need manage messages permission.')
-            .addField(`${pr}prefix [new prefix]`, 'Sets prefix to given symbol after command. If without - shows current prefix.')
-            .addField(`${pr}goal [new goal]`, 'Sets member goal for server to given number after command. If without - shows current goal.');
-        message.channel.send(embed2)
+        let commands = new discord.RichEmbed()
+            .setTitle('🔨 Moderator commands')
+            .setColor('#86ff70')
+            .addField('>addrole @user role name/mention/id', '`>addrole @dank_meme#0001 Staff Team🔧`')
+            .addField('>ban @user/userid reason', '`>ban @dank_meme#0001 gay raider`')
+            .addField('>kick @user reason', '`>kick @dank_meme#0001 chill with the fighting in main chat`')
+            .addField('>mute @user amount_of_minutes reason', '`>mute @dank_meme#0001 60 dude stop spamming`')
+            .addField('>purge amount of messages between 1 and 100', '`>purge 10`')
+            .addField('>removerole @user role name/mention/id', '`>removerole @dank_meme#0001 579324393566502914`')
+            .addField('>unmute @user', '`>unmute @dank_meme#0001`');
+        message.reply(commands)
     }
-    if(args[0] === 'regular') {
-        const embed3 = new discord.RichEmbed()
-            .setTitle('🎲 Regular commands')
-            .setColor('#257682')
-            .setFooter('bot created by dank_meme#0001')
-            .setDescription('<> - required, [] - optional')
-            .addField(`${pr}avatar [mention]`, 'View avatar of given user! If not - shows your avatar!')
-            .addField(`${pr}goal`, 'View current member goal!')
-            .addField(`${pr}commands <category>`, 'View help page of given category commands!')
-            .addField(`${pr}ping`, 'Pong!')
-            .addField(`${pr}prefix`, 'View current bots prefix!')
-            .addField(`${pr}serverinfo`, 'View info about server!')
-            .addField(`${pr}userinfo [mention]`, 'View info about given user, about you if not given!');
-        message.channel.send(embed3)
-    }
-    if(args[0] === 'currency') {
-        const embed4 = new discord.RichEmbed()
-            .setTitle('💵 Currency commands')
-            .setColor('#348743')
-            .setFooter('bot created by dank_meme#0001')
-            .setDescription('<> - required, [] - optional')
-            .addField(`${pr}balance [member]`, 'View your current balance, or balance of given member!')
-            .addField(`${pr}beg`, 'Beg for some money!')
-            .addField(`${pr}buy <item name>`, 'Buy item with provided name! (broken)')
-            .addField(`${pr}inventory [member]`, 'View your inventory, or inventory of given member!')
-            .addField(`${pr}leaderboard`, 'View the richest people across all the servers the bot is in!')
-            .addField(`${pr}redeem <code>`, 'Redeem promocode and get money if you were first!')
-            .addField(`${pr}shop`, 'View shop!')
-            .addField(`${pr}transfer <member> <money>`, 'Send given amount of money to given member!')
-            .addField(`${pr}quiz`, 'Take a quiz and have chance to win 5 coins if guess!');
-        message.channel.send(embed4)
-    }
+
     if(args[0] === 'fun') {
-        const embed5 = new discord.RichEmbed()
-            .setTitle('🎱 Fun commands')
-            .setFooter('bot created by dank_meme#0001')
-            .setColor('#745682')
-            .setDescription('<> - required, [] - optional')
-            .addField(`${pr}b ["embed"]`, '🅱')
-            .addField(`${pr}count [seconds]`, 'Start the downcount from given seconds to zero. If not given starts from 60')
-            .addField(`${pr}scam <item>`, 'Scam guys with person below gets <item> joke (will be fake)!');
-        message.channel.send(embed5)
+        let commands = new discord.RichEmbed()
+            .setTitle('🎲 Fun commands')
+            .setColor('#ef2bff')
+            .addField('>ask question', '`>ask am i gay? :thinking:`')
+            .addField('>b', '`>b`')
+            .addField('>count seconds', '`>count 10`')
+            .addField('>dice sides', '`>dice 6`')
+            .addField('>f', '`>f`')
+            .addField('>flex', '`>flex`')
+            .addField('>meme', '`>meme`')
+            .addField('>scam item to scam', '`>scam nitro`');
+
+        message.reply(commands)
     }
-    if(args[0] === 'developer') {
-        if(message.author.id !== require('../configs/auth').ownerID) return message.channel.send('DEVELOPER DANGER ZONE');
-        const embed6 = new discord.RichEmbed()
-            .setTitle('dev commands')
-            .setColor('#938923')
-            .addField(`${pr}eval <code>`, 'eval some code')
-            .addField(`${pr}status <online/offline/reboot/update>`, 'set bots status')
-            .addField(`${pr}addmoney <userid> <amount>`, 'add money to given user')
-            .addField(`${pr}addpromo <money> <code>`, 'add new promocode');
-        message.channel.send(embed6)
+
+    if(args[0] === 'server') {
+        let commands = new discord.RichEmbed()
+            .setTitle('🔧 Server-related commands')
+            .setColor('#4bffd1')
+            .addField('>announce announcement text', '`>announce We are at 10 members!` [deprecated]')
+            .addField('>giveaway start number_of_hours number_of_winners item to giveaway', '`>giveaway start 24 2 Nitro classic one month! (2 winners!)`')
+            .addField('>gp', '`>gp`')
+            .addField('>serverinfo', '`>serverinfo`')
+            .addField('>suggest', '`>suggest suggestion text` [deprecated]');
+
+        message.reply(commands)
     }
+
+    if(args[0] === 'bot') {
+        let commands = new discord.RichEmbed()
+            .setTitle('🤖 Bot-related commands')
+            .setColor('#74767b')
+            .addField('>commands (category name)', '`>commands mod`')
+            .addField('>help', '`>help`')
+            .addField('>info', '`>info`')
+            .addField('>ping', '`>ping`')
+            .addField('>uptime', '`>uptime`');
+
+        message.reply(commands)
+    }
+
+    if(args[0] === 'regular') {
+        let commands = new discord.RichEmbed()
+            .setTitle('💯 Regular commands')
+            .setColor('#6e2012')
+            .addField('>avatar (@user)', '`>avatar`')
+            .addField('>userinfo (@user)', '`>userinfo @dank_meme#0001`');
+
+        message.reply(commands)
+    }
+
+
 };
 
 exports.help = {
@@ -86,4 +87,4 @@ exports.help = {
     "aliases": "cmds",
     "description": "view needed commands catrgory",
     "usage": ">help [category]"
-}
+};
--- commands/info.js
+++ commands/info.js
@@ -1,16 +1,16 @@
 exports.run = (client, message, args) => {
     const emb = require('discord.js').RichEmbed;
     const embed = new emb()
-        .setTitle('Current version 1.0.5')
-        .setDescription('Version 1.0.5 =>' +
-        '\n**ok**' +
-        '\n> thanks everyone who paid respects to currency using `>f` command' +
-        '\n> now you pay respects to RoDog, because he was demoted :( or be beaned'+
-        '\n> added automod' +
-        '\n> added giveaways 🎉' +
-        '\n> hopefully those things above good working' +
-        '\n> added `>gp` command to do cool giveaway ping :cool: <:awoo:594258019433578508>' +
-        '\n> added limit to use commands (you now need to wait 1.5 seconds between using commands) :cool:')
+        .setTitle('Current version 1.0.6')
+        .setDescription('Version 1.0.6 =>' +
+        '\n**yes**' +
+        '\n> thanks for everyone who paid respects to rodog with `>f`, now pay respects to Banana, his nitro expired' +
+        '\n> added the >giveaway end thing to end giveaway before time ends'+
+        '\n> automod no more triggers you if you say minigame or word which contain niga, also nige, so Nigeria is allowed now' +
+        '\n> `>cmds` command now is so nice' +
+        '\n> deleted useless `>prefix` command for now, because you can\'t change prefix anymore' +
+        '\n> `>addrole` and `>removerole` now can be used with id or mention <:awoo:594258019433578508>' +
+        '\n> you can now set time for `>mute` command')
         .setColor('#477383');
     message.channel.send(embed)
 };
