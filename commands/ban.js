1c4ce0d966e5529091da914022f83d9add28d4d1
Chara1111 <kirill.babikov28@gmail.com>
2019-09-05 22:39:11 +0300

    beta 1.0.7

--- commands/ban.js
+++ commands/ban.js
@@ -1,37 +1,29 @@
 const discord = require('discord.js');
 exports.run = async(client, message, args) => {
     const user = message.mentions.users.first();
-    let banReason = args.slice(1).join(' ');
-    if(!banReason) banReason = 'No reason given.';
     let embed = new discord.RichEmbed()
         .setColor('#ff0000')
         .setTitle('Oops!')
         .setFooter('bot created by dank_meme#0001')
         .setDescription('<:cross:584800355951443968> To ban members, you need to have ban members permission!');
     if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(embed);
-    if (!user) {
-        try {
-            if (!message.guild.members.get(args.slice(0, 1).join(' '))) message.channel.send('Couldn\'t get a Discord user with this userID!')
-            let user1 = message.guild.members.get(args.slice(0, 1).join(' '));
-            let user = user1.user;
-        } catch (error) {
-            return message.reply('Couldn\'t get a Discord user with this userID!');
-        }
+
+    if(!user) {
+        let user = message.guild.members.get(args.shift());
+        if(!user) return message.reply("Couldnt get valid user...")
     }
+
+    let reason = args.join(" ");
+
     if (user === message.author) return message.reply('You can\'t ban yourself');
     if (!message.guild.member(user).bannable) return message.reply('You can\'t ban this user!');
-    if(message.member.highestRole.comparePositionTo(user1.highestRole) < 1) return message.channel.send('<:cross:584800355951443968> dont try to ban person with ur role or above u');
+    if(message.member.highestRole.comparePositionTo(user.highestRole) < 1) return message.channel.send('<:cross:584800355951443968> dont try to ban person with ur role or above u');
 
-    await client.users.get(user.id).send(`You were banned in ${message.guild.name} for: ${banReason}`)
+    await client.users.get(user.id).send(`You were banned in ${message.guild.name} for: ${reason}`);
 
     await message.guild.ban(user);
 
-    const banConfirmationEmbed = new discord.RichEmbed()
-        .setColor('RED')
-        .setDescription(`🔨 ${user.tag} has been successfully banned!`)
-        .addField('Reason:', `${banReason}`);
-    message.channel.send({embed: banConfirmationEmbed});
-
+    await message.channel.send(`**${user.tag} was banned for: ${reason}**`)
 };
 
 exports.help = {
