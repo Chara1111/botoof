d5ae57babc1554a96da29f18e379d48bc988b595
Chara1111 <42031238+Chara1111@users.noreply.github.com>
2020-05-07 19:35:50 +0300

    Update botban.js

--- commands/botban.js
+++ commands/botban.js
@@ -3,7 +3,8 @@ const fs = require('fs')
 exports.run = (client,message,args) => {
 if(message.author.id !== "398181506423455755" && message.author.id !== "612930885230002208") return message.reply("Woah there, you do not have permission to botban people")
 
-let botbanned = (message.mentions.users.first() || client.users.get(args[0]))
+let botbanned = (message.mentions.users.first())
+if(!botbanned) let botbanned = client.users.get(args[0])
 if(!botbanned) return message.reply("Could not find this user, please mention them or enter their user ID")
 
 let bannedlist = JSON.parse(fs.readFileSync("./botbanned.json", "utf8"));
