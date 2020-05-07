2644032fc3a9be7df56e5df9f43458a73273a791
Chara1111 <42031238+Chara1111@users.noreply.github.com>
2020-05-07 19:13:41 +0300

    created botban.js

---
+++ commands/botban.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+exports.run = (client,message,args) => {
+if(message.author.id !== "398181506423455755" && message.author.id !== "612930885230002208") return message.reply("Woah there, you do not have permission to botban people")
+
+let botbanned = (message.mentions.users.first() || client.users.get(args[0]))
+if(!botbanned) return message.reply("Could not find this user, please mention them or enter their user ID")
+
+let bannedlist = JSON.parse(fs.readFileSync("./botbanned.json", "utf8"));
+if bannedlist.includes(botbanned.id) return message.reply("This user is already bot banned!")
+bannedlist.append(botbanned.id)
+fs.writeFile("./botbanned.json", JSON.stringify(bannedlist));
+return message.reply(`Successfully bot banned ${botbanned.tag}`)
+}
