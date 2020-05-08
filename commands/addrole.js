f8c020f5c6670446b4492342b55bdba255b1a494
Chara1111 <42031238+Chara1111@users.noreply.github.com>
2020-05-08 18:01:02 +0300

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -8,9 +8,9 @@ exports.run = async(client, message, args) => {
     if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(embed);
     if(!args[0]) return message.channel.send('Please mention valid user.');
     let role;
-    if(message.mentions.roles.first()) let role = message.mentions.roles.first();
-    else if(message.guild.roles.get(args[1])) let role = message.guild.roles.get(args[1]);
-    else if(message.guild.roles.find(r => r.name === args[1])) let role = message.guild.roles.find(r => r.name === args[1]);
+    if(message.mentions.roles.first()) role = message.mentions.roles.first();
+    else if(message.guild.roles.get(args[1])) role = message.guild.roles.get(args[1]);
+    else if(message.guild.roles.find(r => r.name === args[1])) role = message.guild.roles.find(r => r.name === args[1]);
     else return message.channel.send("Please mention valid role/say it name/id");
     if(message.member.highestRole.comparePositionTo(role) < 1) return message.channel.send('Uh oh, to assign roles, they need to be below your highest role!');
     let user = message.mentions.users.first().id;
