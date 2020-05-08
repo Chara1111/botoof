b1ebc81699dd5f721fa119bf472af571cd3ad2e5
Chara1111 <42031238+Chara1111@users.noreply.github.com>
2020-05-08 18:05:40 +0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,7 +1,10 @@
 const discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-    let mr = '570611175407222794';
+    let mr;
+    mr = message.guild.roles.find(r => r.name.toLowerCase() === "muted")
+    if(!mr) mr = message.guild.roles.find(r => r.name.toLowerCase().includes("mute"))
+    if(!mr) return message.reply("Could not find muted role. Please create a role with name \"Muted\" or edit name of existing one.")
     if(!args[1]) return message.reply("Please specify mute time in minutes (e.g. 15)");
     if(isNaN(Number(args[1]))) return message.reply("Please specify __valid__ mute time in minutes (e.g. 15)");
     let mutetime = Number(args[1]);
@@ -11,7 +14,7 @@ exports.run = (client, message, args) => {
         .setFooter('bot created by dank_meme#0001')
         .setDescription('<:cross:584800355951443968> To mute members, you need to have manage channels permission!');
     if(!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send(embed);
-    if(message.member.highestRole.comparePositionTo(message.guild.members.get(message.mentions.users.first().id).highestRole) < 0) return message.channel.send('<:cross:584800355951443968> lmfao dont try to mute person with ur role or above u');
+    if(message.member.highestRole.comparePositionTo(message.guild.members.get(message.mentions.users.first().id).highestRole) < 0) return message.channel.send('<:cross:584800355951443968> You cannot mute person with same role as you or higher.');
     message.guild.members.get(message.mentions.users.first().id).addRole(mr)
         .then(() => {message.channel.send(`<:tick:584800524000296971> Successfully muted ${message.mentions.users.first().tag}`);
         setTimeout(async function() {await message.guild.members.get(message.mentions.users.first().id).removeRole(mr)}, mutetime*60000)})
