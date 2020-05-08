e24485454e6bd800ed2404b7677a06bb50bad788
Chara1111 <42031238+Chara1111@users.noreply.github.com>
2020-05-08 14:02:12 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -2,14 +2,9 @@ exports.run = (client, message, args) => {
     if(message.channel.id === "594518913170145280") return message.reply("Nope, please go to bot commands ;)")
     const emb = require('discord.js').RichEmbed;
     const embed = new emb()
-        .setTitle('Current version 1.0.8')
-        .setDescription('Version 1.0.8 =>' +
-        '\n**boi birthday command was without errors first time I runned it**' +
-        '\n> `>bday` command! (Warning: it doesnt show real birthday, but acoount birthday)' +
-        '\n> `>coinflip` is new command too.'+
-        '\n> Secret command is being worked! Coming soon...' +
-        '\n> Minecraft command is being worked too. Stay tuned.' +
-        '\nTell me thanks for releasing update right now and not after I will done making 2 commands above')
+        .setTitle('Current version 1.0.9')
+        .setDescription('Version 1.0.9 =>' +
+        '\nChangelog is gone (i meant it would be here in next version)')
         .setColor('#477383');
     message.channel.send(embed)
 };
